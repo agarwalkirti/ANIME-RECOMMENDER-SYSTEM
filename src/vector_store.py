4c33e4d1de4f663f5fd2d94d85d8e3770b6a1940
agarwalkirti <akirti302@gmail.com>
2025-12-13 15:30:46 +0530

    updated vector store file

--- src/vector_store.py
+++ src/vector_store.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 
 class VectorStoreBuilder:
     def __init__(self,csv_path:str,persist_dir:str="chroma_db"):
